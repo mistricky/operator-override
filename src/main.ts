b35df80b9ee5197e34d2d74079591cc467b9e973
HaoDaWang <1306036576@qq.com>
2019-07-10 10:12:11 +0800

    [Feat] remove code for test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import {OperatorOverride} from './override';
-import { transform } from '@babel/core';
-import * as FS from 'fs'
 
 const plugin = () => new OperatorOverride().dist();
 
 export = plugin
 
-const out = transform(FS.readFileSync("../example/src.js").toString(), {
-  plugins: [
-      ["@babel/plugin-syntax-decorators", {legacy: true}],
-      plugin,
-    ]
-  })
+// const out = transform(FS.readFileSync("../example/src.js").toString(), {
+//   plugins: [
+//       // ["@babel/plugin-syntax-decorators", {legacy: true}],
+//       [plugin, {legacy: true}],
+//     ]
+//   })
 
-FS.writeFileSync("../example/out.js", out && out.code)
+// FS.writeFileSync("../example/out.js", out && out.code)
